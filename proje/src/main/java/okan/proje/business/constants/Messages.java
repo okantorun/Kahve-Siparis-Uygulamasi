9d467568ce3e6fff89c62026f0f469c816f750d6
Okan Torun <torun.okn@gmail.com>
2022-08-28 11:25:34 +0300

    Operasyonları için gerekn mesajlar için için mesaj sınıfı oluşturuldu

---
+++ proje/src/main/java/okan/proje/business/constants/Messages.java
@@ -0,0 +1,8 @@
+package okan.proje.business.constants;
+
+public final class Messages {
+	public final String coffeeAdded = "Kahve ekleme işlemi başarılı";
+	public final String coffeeUpdated = "Kahve güncelleme işlemi başarılı";
+	public final String coffeeDeleted = "Kahve silme işlemi başarılı";
+	public final String insufficientIngredient = "İstenilen ürün için malzeme miktarı yetersizdir";
+}
